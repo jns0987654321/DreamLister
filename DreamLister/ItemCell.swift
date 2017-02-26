edc3a373d5fea1873c07e95d535e22352d0a70e7
Junior Samaroo <jns0987654321@gmail.com>
2017-02-26 18:03:15 -0500

    More boilerplate setup

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Junior Samaroo on 2017-02-26.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
